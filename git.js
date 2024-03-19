e2d6f06ea6c083fe526c7c8f7ecd92fb63ab743f
Khaira <hutamikhairatul@gmail.com>
2024-03-19 21:02:20 +0700

    v1.0

--- git.js
+++ git.js
@@ -1,3 +1,11 @@
 // Fungsi untuk menjalankan kalkulator
 function hitung(operator, angka1, angka2) {
     let hasil;
+
+    //switch case untuk menentukan operasi metamatika berdasarkan operator
+    switch (operator) {
+        case '+':
+            //operasi penambahan 
+            hasil = angka1 + angka2;
+            break;
+    }
